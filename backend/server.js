eff305387e1aa29db48a0fbec0acb8b09a47bb63
Harshrajsinh Solanki <harshrajsinhsolanki11@gmail.com>
2025-12-27 09:33:56 +0530

    Backend + Express Server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("Odoo Hackathon Backend is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
